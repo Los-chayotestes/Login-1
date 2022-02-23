4aa82059d1019642ee0651972b60d3c8993a84c7
saulherm <saulfexslo@gmail.com>
2022-02-23 10:54:35 -0600

    It was a modification to login signupc and welcom

---
+++ login/welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <?php require'partials/link.php' ?>
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+      <h1>Welcome</h1>
+      <br>you are succesfully logged in
+
+    </body>
+  </html>
