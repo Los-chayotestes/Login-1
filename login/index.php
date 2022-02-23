cc421bb5d40687d0d20a7ccdf9a0edf0d7b1b6d4
saulherm <saulfexslo@gmail.com>
2022-02-23 11:01:14 -0600

    Change on index.php

---
+++ login/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+
+  </head>
+  <body>
+    <center>
+      <h3>Login Here</h3>
+      <form action="Login.php" method= "post">}
+        <table>
+          <tr>
+            <td>Username:</td>
+            <td> <input type="text" name="user" ></td>
+          </tr>
+          <tr>
+            <td>Password:</td>
+            <td> <input type="password" name="user_pass" ></td>
+          </tr>
+          <tr>
+            <td><input type="submit" name="submit" value="Login"> </td>
+          </tr>
+          <tr>
+            <td>Not yet member <a href="">Register</a> </td>
+          </tr>
+        </table>
+    </center>
+  </body>
+</html>
